97a6c1f0ec87aad2df71a352f03de2139033ec38
nilson <josenilsonsousa366@gmail.com>
2025-08-04 21:40:11 -0300

    atv2 dia 04/08

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,62 @@
 // import NavBar from "@/components/NavBar"
 import AtvCard from "@/components/CartAtv"
 
+const CardData = [
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Filme',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Serie',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Filme',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Serie',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Livro',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Livro',
+    },
+]
+
 export default function About(){
 
     return(
         <div>
-            {/* <NavBar label1="Produtos" label2="Adminitradores" label3="Time"/> */}
-            <h1>Sobre nós</h1>
-            <p>Informações sobre a empresa ...</p>
-            <AtvCard 
-               url='https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg'
-               title='Lindo Coqueiro'
-               description='O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!'
-               />
+            <div className="flex flex-row justify-center gap-8 flex-wrap my-20">
+                {CardData.map((card, index) => (
+                    <AtvCard 
+                    key={index}
+                    url={card.url}
+                    title={card.title}
+                    description={card.description}
+                    categoria={card.categoria}
+                    />
+                ))}
+
+            </div>
+
         </div>
     )
 }
--- src/components/CartAtv.tsx
+++ src/components/CartAtv.tsx
@@ -1,27 +1,35 @@
 
 
 
-type ImageProps = {
+type CardProps = {
     url: string
     title: string
     description: string
+    categoria: string
 
 }
 
-export default function AtvCard({url, title, description} : ImageProps) {
+
+
+
+
+export default function AtvCard({url, title, description, categoria} : CardProps) {
     return (
-        <div className="bg-amber-300 ml-[300px] pb-2.5 max-w-[600px] rounded-2xl">
-            <div>
-                <img 
-                src={url}
-                alt="sla"
-                width={600}
-                height={600}
-                className="rounded-t-2xl"
-                />
+        <>
+            <div className="bg-amber-300 pb-2.5 max-w-[400px] rounded-2xl">
+                <div>
+                    <img 
+                    src={url}
+                    alt="sla"
+                    width={400}
+                    height={400}
+                    className="rounded-t-2xl"
+                    />
+                </div>
+                <h2 className="px-3 text-4xl my-1">{title}</h2>
+                <p className="px-3">{description}</p>
+                <p className="px-3 font-bold text-red-600 text-[12px]">{categoria}</p>
             </div>
-            <h2 className="px-3 text-4xl my-1">{title}</h2>
-            <p className="px-3">{description}</p>
-        </div>
+        </>
     )
 }
