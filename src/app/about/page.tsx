5116d0a5fe325f212cc0eb8fcc8dc86591b2c266
nilson <josenilsonsousa366@gmail.com>
2025-08-06 21:25:25 -0300

    atv1 dia 06/08

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,57 +1,52 @@
-"use client";
+'use client'
 
-import AtvCard from "@/components/CartAtv";
-import Counter from "@/components/Counter";
+import AtvCard from "@/components/CartAtv"
+import Counter from "@/components/Counter"
 
 const CardData = [
-  {
-    url: "https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg",
-    title: "Lindo Coqueiro",
-    description:
-      "O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!",
-    categoria: "Filme",
-  },
-  {
-    url: "https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg",
-    title: "Lindo Coqueiro",
-    description:
-      "O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!",
-    categoria: "Serie",
-  },
-  {
-    url: "https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg",
-    title: "Lindo Coqueiro",
-    description:
-      "O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!",
-    categoria: "Filme",
-  },
-  {
-    url: "https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg",
-    title: "Lindo Coqueiro",
-    description:
-      "O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!",
-    categoria: "Serie",
-  },
-  {
-    url: "https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg",
-    title: "Lindo Coqueiro",
-    description:
-      "O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!",
-    categoria: "Livro",
-  },
-  {
-    url: "https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg",
-    title: "Lindo Coqueiro",
-    description:
-      "O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!",
-    categoria: "Livro",
-  },
-];
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Filme',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Serie',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Filme',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Serie',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Livro',
+    },
+    {
+        url: 'https://botanicaipe.com.br/wp-content/uploads/2022/11/coqueiro-gigante-com-folhas-balancando-ao-vento.jpg',
+        title: 'Lindo Coqueiro',
+        description: 'O coqueiro mais lindo e mais verde que você vai ver em toda sua vida. Este possui a melhor água de coco do mundo!',
+        categoria: 'Livro',
+    },
+]
 
-export default function About() {
-  return (
-    <div>
-      <div className="flex flex-row justify-center gap-8 flex-wrap my-20">
+export default function About(){
+
+    return(
+        <div>
+            {/* <div className="flex flex-row justify-center gap-8 flex-wrap my-20">
                 {CardData.map((card, index) => (
                     <AtvCard 
                     key={index}
@@ -62,10 +57,13 @@ export default function About() {
                     />
                 ))}
 
+            </div> */}
+            <div className="flex justify-center my-[80px]">
+                <Counter
+                    ititial={0}
+                />
             </div>
-      <div className="flex flex-col justify-center my-[80px]">
-        <Counter ititial={0} />
-      </div>
-    </div>
-  );
+
+        </div>
+    )
 }
